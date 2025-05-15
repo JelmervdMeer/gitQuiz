9b5f6cf7b25f82f3b6208d0b4a18bf6c5eb5da36
Jelmer van der Meer <jelmervandermeer02@gmail.com>
2025-05-15 14:08:54 +0200

    javascript bestand aangemaakt met daarin de json benodigd voor de quiz

---
+++ quizJava.js
@@ -0,0 +1,86 @@
+var mijnVragen = [
+	{
+		vraag: "Hier sla je alle bestanden van je project op.",
+		antwoorden: {
+			a: 'Commit',
+			b: 'Repository',
+			c: 'Branch',
+			d: 'Push'
+		},
+		correcteAntwoord: 'b'
+	},
+	{
+		vraag: "Hoe heet een aparte lijn van ontikkeling binnen je project?",
+		antwoorden: {
+			a: 'Commit',
+			b: 'Pull',
+			c: 'Branch',
+			d: 'Push'
+		},
+		correctAntwoord: 'c'
+	},
+	{
+		vraag: "Het haalt de laatste wijzingen op en voegt ze samen met je lokale branch<",
+		antwoorden: {
+			a: 'Commit',
+			b: 'Pull',
+			c: 'Branch',
+			d: 'Push'
+		},
+		correcteAntwoord: 'b',
+	},
+	{
+		vraag:"Dit slaat je wijzigingen op in je lokale repositery",
+		antwoorden: {
+			a: 'Commit',
+			b: 'Pull',
+			c: 'Branch',
+			d: 'Push'
+		},
+		correctAntwoord: 'a',
+		
+	},
+	{
+		vraag:"Dit upload je wijzigingen naar de remote repositery",
+		antwoorden: {
+			a: 'Commit',
+			b: 'Pull',
+			c: 'Branch',
+			d: 'Push'
+		},
+		correcteAntwoord: 'd',
+	},
+	{
+		vraag: "Dit maakt de wijzigingen zichtbaar voor andere mensen die aan het project werken",
+		antwoorden: {
+			a: 'Commit',
+			b: 'Pull',
+			c: 'Branch',
+			d: 'Push'
+		},
+		correcteAntwoord: 'd',
+	},
+	{
+		vraag: "Dit maakt een kopie van de repository op je computer",
+		antwoorden: {
+			a: 'Commit',
+			b: 'Clone',
+			c: 'Branch',
+			d: 'Push'
+		},
+		correcteAntwoord: 'b',
+	},
+	{
+		vraag: "Dit maakt een nieuwe versie van je project aan",
+		antwoorden: {
+			a: 'Commit',
+			b: 'Pull',
+			c: 'Branch',
+			d: 'Push'
+		},
+		correcteAntwoord: 'a',
+	}
+
+
+
+];
